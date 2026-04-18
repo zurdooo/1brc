ee4ac38e85919b91ff2b625ad4e55b0a1bb5b96d
zurdooo <135689466+zurdooo@users.noreply.github.com>
2026-04-18 14:31:50 -0400

    Finished custom hashmap implementation

--- cpp/src/1brc.cpp
+++ cpp/src/1brc.cpp
@@ -18,9 +18,9 @@ aggregate, and retrieve the results per station.
 access value by name if not present, append to total, add 1 to struct count
 
 TODO LIST
-- Lets switch to std::span when we have a ptr and size together
-- Implement custom hashmap with linear probing and transparent key lookup
 - Add thread logic, split the file into N parts and have each thread process a part, then aggregate results at the end
+    - For threads go with an atomic iterator that goes through threads, anytime this atomic var gets called, it feeds a chunk
+    - To the requesting thread
 - MAYBE: Implement a better parsing function
 */
 
@@ -39,72 +39,147 @@ TODO LIST
 #include <chrono>
 #include <span>
 
-/// Hash to use with transparent key lookup in unordered_map, allows for efficient lookup using stringview and only allocate key once
-struct KeyHash
-{
-    using is_transparent = void;
-    // Allows for string views, so we can lookup without allocating a new string
-    size_t operator()(std::string_view key) const noexcept
-    {
-        return std::hash<std::string_view>{}(key);
-    }
-    // Allows for strings
-    size_t operator()(const std::string &key) const noexcept
-    {
-        return std::hash<std::string_view>{}(key);
-    }
-};
-
 /// @brief Represents the weather station found in "measurements.txt"
 struct WeatherStation
 {
-    // * Added for hashmap parsing
-    const char *key; // Pointer to its name in mmap
-    uint_fast16_t len;    // Length of name
+    std::string_view key;
 
-    int_fast16_t min = 999;
-    int_fast16_t max = -999;
-    uint_fast32_t total = 0;
-    uint_fast16_t count = 0; // Total instances of measurements we read
-};
+    int_fast16_t min;
+    int_fast16_t max;
+    int_fast32_t total;
+    uint_fast32_t count; // Total instances of measurements we read
 
-struct HashMan
-{
-    WeatherStation *stations;
-    uint32_t *hashes; // Stores the hash of a station key
-
-    uint_fast16_t capacity; // Power of 2
-    uint_fast16_t mask;     // Capacity - 1, for bitmasking instead of modulo operation
-
-    // TODO: Get or Create an entry, Lookup & Insert
-    // TODO: Update method if we have a hit instead of a creation
-    // TODO: Constructor/Setup
-    // Looks for value in table,
-    WeatherStation *get_or_create(const char *key, uint_fast16_t key_len, uint32_t key_hash)
+    // Init the key into struct, plus defaults
+    void init(std::string_view key)
     {
+        this->key = key;
+
+        min = 999;
+        max = -999;
+        total = 0;
+        count = 0;
+    }
+
+    //  Char by char comparison
+    bool equals(std::string_view other_key) const
+    {
+        return key == other_key;
     }
 };
 
 // FNV-1a 32-bit hash function
 // Generate a deterministic integer hash of a byte seq
-static inline uint32_t FNVmanhash(const char *s, uint_fast8_t keylen)
+static inline uint_fast32_t FNVmanhash(std::string_view key)
 {
     uint_fast32_t hash_number = 2166136261u; // FNV Hash number, good for distributions and stuff
-    for (int i = 0; i < keylen; i++)
+    size_t key_len = key.length();
+    for (size_t i = 0; i < key_len; i++)
     {
-        hash_number ^= (uint8_t)s[i]; // Update byte into number-state
-        hash_number *= 16777619;      // Mix bits via multiplication (FNV prime) for avalanche effect
+        hash_number ^= (uint8_t)key[i]; // Update byte into number-state
+        hash_number *= 16777619;        // Mix bits via multiplication (FNV prime) for avalanche effect
     }
     return hash_number;
 }
 
-// Alias for the weather station map
-using StationMap = std::unordered_map<std::string, WeatherStation, KeyHash, std::equal_to<>>;
+// TODO: Add a move function
+// TODO: Add a const non mutating find
+struct HashMan
+{
+    WeatherStation *stations;
+    uint_fast32_t *hashes; // Stores the hash of a station key
+
+    uint_fast16_t size;
+    uint_fast16_t capacity;
+    uint_fast16_t mask; // Bitmasking instead of modulo operation
+
+    // Default Constructor
+    HashMan()
+    {
+        capacity = 16384; // WE KNOW THE CAPACITY is 10,000 beforehand so we can go to the nearest power of 2 greater than 10,000, 2 ^ 14
+        mask = capacity - 1;
+        size = 0;
+
+        // * Malloc enough slots for capacity number of WeatherStation elements
+        stations = new WeatherStation[capacity]();
+        hashes = new uint_fast32_t[capacity]();
+    }
+
+    // Destructor
+    ~HashMan()
+    {
+        delete[] stations;
+        delete[] hashes;
+    }
+
+    // Move Constructor
+    HashMan(HashMan &&other) noexcept : stations(other.stations), hashes(other.hashes), size(other.size), capacity(other.capacity), mask(other.mask)
+    {
+        other.stations = nullptr;
+        other.hashes = nullptr;
+        other.size = 0;
+        other.capacity = 0;
+        other.mask = 0;
+    }
+
+    // * Disables copying
+    HashMan(const HashMan &) = delete;
+    HashMan &operator=(const HashMan &) = delete;
+
+    // TODO: Quadratic Probing approach?
+    // TODO: Fix control flow in potential match
+    // Looks for value in table, linear probing approach
+    WeatherStation *get_or_create(std::string_view key)
+    {
+        // Shift by 1, so 0 can remain the empty slot flag
+        uint_fast32_t key_hash = FNVmanhash(key) + 1;
+        uint_fast32_t idx = key_hash & mask;
+
+        // Probe for empty slot
+        while (true)
+        {
+            // Empty Insert
+            if (hashes[idx] == 0)
+            {
+                size++;
+                hashes[idx] = key_hash;
+                // Initialize entry
+                stations[idx].init(key);
+
+                return &stations[idx];
+            }
+            // Potential match
+            else if (hashes[idx] == key_hash && stations[idx].equals(key))
+            {
+                return &stations[idx];
+            }
+
+            // Otherwise linear probe forward
+            idx = (idx + 1) & mask;
+        }
+    }
+
+    // Retrieves the value, immutable
+    const WeatherStation *get(std::string_view key) const
+    {
+        // Shift by 1, so 0 can remain the empty slot flag
+        uint_fast32_t key_hash = FNVmanhash(key) + 1;
+        uint_fast32_t idx = key_hash & mask;
+
+        while (true)
+        {
+            if (hashes[idx] == key_hash && stations[idx].equals(key))
+            {
+                return &stations[idx];
+            }
+            idx = (idx + 1) & mask;
+        }
+        return &stations[idx];
+    }
+};
 
 /// @brief The return type for mmap, so we can cleanup memory after getting the data
 struct MMapFile
 {
-    int fd = -1;
     size_t size = 0;
     const char *data = nullptr;
 
@@ -112,8 +187,8 @@ struct MMapFile
     MMapFile() = default;
 
     // Constructor to initialize the MMapFile with given values
-    MMapFile(int fd_in, size_t size_in, const char *data_in)
-        : fd(fd_in), size(size_in), data(data_in)
+    MMapFile(size_t size_in, const char *data_in)
+        : size(size_in), data(data_in)
     {
     }
 
@@ -124,25 +199,22 @@ struct MMapFile
         {
             ::munmap(const_cast<char *>(data), size);
         }
-        if (fd >= 0)
-        {
-            ::close(fd);
-        }
     }
 
     // Delete copy constructor and copy assignment operator to prevent copying of MMapFile instances, since they manage resources that should not be duplicated
     MMapFile(const MMapFile &) = delete;            // Dont allow the copy constructor so MmapFile a = b is not allowed
     MMapFile &operator=(const MMapFile &) = delete; // Dont allow the copy assignment operator so MmapFile a; a = b is not allowed
 
+    // * Move Constructor
     // Allowing us to move MMapFile instances, transferring ownership of the resources without copying
     MMapFile(MMapFile &&other) noexcept
-        : fd(other.fd), size(other.size), data(other.data)
+        : size(other.size), data(other.data)
     {
-        other.fd = -1;
         other.size = 0;
         other.data = nullptr;
     }
 
+    // * Move assignment
     // Move assignment operator to transfer ownership of resources from one MMapFile instance to another, ensuring proper cleanup of existing resources before taking ownership of the new ones
     // Make sure we have unique ownership
     MMapFile &operator=(MMapFile &&other) noexcept
@@ -156,16 +228,9 @@ struct MMapFile
         {
             ::munmap(const_cast<char *>(data), size);
         }
-        if (fd >= 0)
-        {
-            ::close(fd);
-        }
-
-        fd = other.fd;
         size = other.size;
         data = other.data;
 
-        other.fd = -1;
         other.size = 0;
         other.data = nullptr;
         return *this;
@@ -188,6 +253,9 @@ MMapFile mmap_file()
         std::perror("open");
         return {};
     }
+
+    // * Kernel advise
+    ::posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
 
     auto t1 = std::chrono::high_resolution_clock::now();
     std::println("open() time: {:.6f} seconds",
@@ -215,21 +283,20 @@ MMapFile mmap_file()
 
     // Call mmap and get the pointer to the data
     void *ptr = ::mmap(nullptr, size, PROT_READ, MAP_PRIVATE, fd, 0);
-
     if (ptr == MAP_FAILED)
     {
         std::perror("mmap");
         ::close(fd);
         return {};
     }
+    ::close(fd);
 
     auto t3 = std::chrono::high_resolution_clock::now();
     std::println("mmap() time: {:.6f} seconds",
                  std::chrono::duration<double>(t3 - t2).count());
 
-    // TODO: Switch to std span
     // Return mmap struct
-    return {fd, size, static_cast<const char *>(ptr)};
+    return {size, static_cast<const char *>(ptr)};
 }
 
 // @brief Parses the temperature value from a semicolon pointer and returns scaled int
@@ -300,29 +367,28 @@ void parse_line(const char *&iter, std::string_view &out_name, int_fast16_t &out
     out_value = parse_value(sc);
 }
 
-void add_station(std::string_view name, int_fast16_t value, StationMap &weather_stations)
+// @brief Adds station to map and updates its data with value
+void add_station(std::string_view name, int_fast16_t value, HashMan &weather_stations)
 {
-    auto it = weather_stations.find(name);
-    if (it == weather_stations.end())
-    {
-        it = weather_stations.emplace(std::string(name), WeatherStation{}).first;
-    }
+    auto station_ptr = weather_stations.get_or_create(name);
 
-    auto &ws = it->second;
+    // Grab reference from pointer
+    auto &station = *station_ptr;
 
-    if (value < ws.min)
-        ws.min = value;
-    else if (value > ws.max)
-        ws.max = value;
-    ws.total += value;
-    ws.count++;
+    // TODO: move to weather staion method
+    if (value < station.min)
+        station.min = value;
+    else if (value > station.max)
+        station.max = value;
+
+    station.total += value;
+    station.count++;
 }
 
-StationMap create_weather_station_map(MMapFile &mapped)
+// @brief Parse and map creation loop
+HashMan create_weather_station_map(MMapFile &mapped)
 {
-    // Reserve 10000 slots for our unique stations
-    StationMap weather_stations{};
-    weather_stations.reserve(10000);
+    HashMan weather_stations{};
 
     const char *iter = mapped.data;
     const char *end = mapped.data + mapped.size;
@@ -347,37 +413,46 @@ StationMap create_weather_station_map(MMapFile &mapped)
 }
 
 /// This function should take in the hashmap of all stations and output in the desired format to stdout
-void output_stations(const StationMap &map)
+void output_stations(const HashMan &map)
 {
-    /// Collect and sort keys
-    std::vector<std::string> keys;
-    keys.reserve(map.size());
-    for (auto &[key, _] : map)
-        keys.push_back(key);
-    std::sort(keys.begin(), keys.end());
+    /// Collect and sort indices
+    std::vector<uint_fast16_t> indices;
+    indices.reserve(map.size);
+    for (uint_fast16_t idx = 0; idx < map.capacity; ++idx)
+    {
+        // We have a hit
+        if (map.hashes[idx] != 0)
+        {
+            indices.push_back(idx);
+        }
+    }
+
+    // Comparator as we store indices not the keys
+    std::sort(indices.begin(), indices.end(), [&](uint_fast16_t a, uint_fast16_t b)
+              { return map.stations[a].key < map.stations[b].key; });
 
     // Output
     std::print("{{");
-    for (size_t i = 0; i < keys.size(); ++i)
+    for (size_t i = 0; i < indices.size(); ++i)
     {
-        // TODO: Fix this?
-        auto &ws = map.at(keys[i]);
+        const WeatherStation &ws = map.stations[indices[i]];
+
         int64_t sum = ws.total;
         if (sum > 0)
             sum += ws.count / 2; // rounding
         else
             sum -= ws.count / 2;
-        double mean = static_cast<double>(sum / ws.count);
+        double mean = static_cast<double>(sum) / static_cast<double>(ws.count);
 
         // Print to 1 decimal place
         std::print("{}={:.1f}/{:.1f}/{:.1f}",
-                   keys[i],
+                   ws.key,
                    static_cast<double>(ws.min) / 10.0,
                    mean / 10.0,
                    static_cast<double>(ws.max) / 10.0);
 
         // Print comma separator if not the last element
-        if (i + 1 < keys.size())
+        if (i + 1 < indices.size())
             std::print(", ");
     }
     // Print closing brace, }}
@@ -403,7 +478,7 @@ int main()
     std::println("File read time: {:.6f} seconds", read_time.count());
 
     // Parse file into sorted key hashmap
-    StationMap weather_stations = create_weather_station_map(mapped);
+    HashMan weather_stations = create_weather_station_map(mapped);
 
     auto created_map = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> map_creation_time = created_map - start;
